288e085e182b8e4090252374ae063340b108c493
naser23 <naserabdulkadir0@gmail.com>
2022-08-04 13:32:21 -0400

    Learned that you have to prefix any .env varriable that you use in React with REACT_APP_ in order for it to show up. Was stuck on that recently.

--- Backend/server.js
+++ Backend/server.js
@@ -1,1 +1,9 @@
-console.log("Server...");
+const express = require("express");
+const dotenv = require("dotenv").config();
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
