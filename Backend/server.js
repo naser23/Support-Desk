a7ef845f5f27f8e4cf67abdd44d86f3cc13ad466
naser23 <naserabdulkadir0@gmail.com>
2022-08-04 14:13:35 -0400

    Setting up routes and middleware while also testing some things out with postman.

--- Backend/server.js
+++ Backend/server.js
@@ -1,9 +1,20 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
+const { errorHandler } = require("./middleware/errorMiddleware");
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.get("/", (req, res) => {
-  res.send("Hello");
+  res.status(200).json({ message: "This message should go through now." });
 });
+
+// Routes
+app.use("/api/users", require("./routes/userRoutes"));
+
+// keep errorHander middleware below the route
+app.use(errorHandler);
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
