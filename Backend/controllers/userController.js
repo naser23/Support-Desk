017184e5270969bea5cb500908b5ebee377a2c9a
naser23 <naserabdulkadir0@gmail.com>
2022-08-11 14:02:17 -0400

    Added more to the getMe function in userController.

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -70,7 +70,12 @@ const loginUser = asyncHandler(async (req, res) => {
 // @route /api/users/me
 // @access Private
 const getMe = asyncHandler(async (req, res) => {
-  res.send("me");
+  const user = {
+    id: req.user._id,
+    name: req.user.name,
+    email: req.user.email,
+  };
+  res.status(200).json(user);
 });
 
 // Generate token
