99bfaa9d14c20c4c4731ff2f0e0f21f011b0dc11
naser23 <naserabdulkadir0@gmail.com>
2022-08-16 14:04:51 -0400

    Ticket backend functionality almost done. Tickets can be retrieved, created, updated and deleted through the backend. Will work on getting that functionality working through the front end soon.

--- Backend/controllers/ticketController.js
+++ Backend/controllers/ticketController.js
@@ -1,22 +1,149 @@
 const asyncHandler = require("express-async-handler");
 const User = require("../models/userModel");
 const Ticket = require("../models/ticketModel");
+const { update } = require("../models/userModel");
 
 // @desc Get user tickets
 // @route GET /api/tickets
 // @access Private
 const getTickets = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "getTickets" });
+  // Get user using the id in the JWT
+  const user = await User.findById(req.user.id);
+
+  if (!user) {
+    res.status(401);
+    throw new Error("User not found");
+  }
+
+  const tickets = await Ticket.find({ user: req.user.id });
+  res.status(200).json(tickets);
+});
+
+// @desc Get single ticket
+// @route GET /api/tickets/:id
+// @access Private
+const getUserTicket = asyncHandler(async (req, res) => {
+  // Get user using the id in the JWT
+  const user = await User.findById(req.user.id);
+
+  if (!user) {
+    res.status(401);
+    throw new Error("User not found");
+  }
+
+  const ticket = await Ticket.findById(req.params.id);
+
+  if (!ticket) {
+    res.status(404);
+    throw new Error("Ticket not found");
+  }
+
+  // See if the ticket belongs to the same user
+  if (ticket.user.toString() !== req.user.id) {
+    res.status(401);
+    throw new Error("Not Authorized");
+  }
+
+  res.status(200).json(ticket);
 });
 
 // @desc Create new ticket
 // @route POST /api/tickets
 // @access Private
 const createTickets = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "createTickets" });
+  const { product, description } = req.body;
+
+  if (!product || !description) {
+    res.status(400);
+    throw new Error("Please add a product and description");
+  }
+
+  // Get user using the id in the JWT
+  const user = await User.findById(req.user.id);
+
+  if (!user) {
+    res.status(401);
+    throw new Error("User not found");
+  }
+
+  const ticket = await Ticket.create({
+    product,
+    description,
+    user: req.user.id,
+    status: "new",
+  });
+
+  res.status(201).json(ticket);
+});
+
+// @desc Delete single ticket
+// @route DELETE /api/tickets/:id
+// @access Private
+const deleteTicket = asyncHandler(async (req, res) => {
+  // Get user using the id in the JWT
+  const user = await User.findById(req.user.id);
+
+  if (!user) {
+    res.status(401);
+    throw new Error("User not found");
+  }
+
+  const ticket = await Ticket.findById(req.params.id);
+
+  if (!ticket) {
+    res.status(404);
+    throw new Error("Ticket not found");
+  }
+
+  // See if the ticket belongs to the same user
+  if (ticket.user.toString() !== req.user.id) {
+    res.status(401);
+    throw new Error("Not Authorized");
+  }
+
+  await ticket.remove();
+
+  res.status(200).json({ success: true });
+});
+
+// @desc Update ticket
+// @route PUT /api/tickets/:id
+// @access Private
+const updateTicket = asyncHandler(async (req, res) => {
+  // Get user using the id in the JWT
+  const user = await User.findById(req.user.id);
+
+  if (!user) {
+    res.status(401);
+    throw new Error("User not found");
+  }
+
+  const ticket = await Ticket.findById(req.params.id);
+
+  if (!ticket) {
+    res.status(404);
+    throw new Error("Ticket not found");
+  }
+
+  // See if the ticket belongs to the same user
+  if (ticket.user.toString() !== req.user.id) {
+    res.status(401);
+    throw new Error("Not Authorized");
+  }
+
+  const updatedTicket = await Ticket.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    { new: true }
+  );
+
+  res.status(200).json(updatedTicket);
 });
 
 module.exports = {
   getTickets,
   createTickets,
+  getUserTicket,
+  deleteTicket,
+  updateTicket,
 };
--- Backend/routes/ticketRoutes.js
+++ Backend/routes/ticketRoutes.js
@@ -4,8 +4,17 @@ const { protect } = require("../middleware/authMiddleware");
 const {
   getTickets,
   createTickets,
+  getUserTicket,
+  deleteTicket,
+  updateTicket,
 } = require("../controllers/ticketController");
 
 router.route("/").get(protect, getTickets).post(protect, createTickets);
+
+router
+  .route("/:id")
+  .get(protect, getUserTicket)
+  .delete(protect, deleteTicket)
+  .put(protect, updateTicket);
 
 module.exports = router;
