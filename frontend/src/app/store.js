9ab950fc6ae9c403103c9010d7d7efd4e310ecd6
naser23 <naserabdulkadir0@gmail.com>
2022-08-17 14:11:49 -0400

    Brought in the state for tickets into redux.

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/auth/authSlice";
+import ticketReducer from "../features/tickets/ticketSlice";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
-    //+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    ticket: ticketReducer,
   },
 });
+//+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
